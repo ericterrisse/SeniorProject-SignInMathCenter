fb228d6c44d67a838e02e236dbcff6b9326b20be
Eric Terrisse <eterrissebordas@gmail.com>
2024-03-12 10:55:44 -0500

    create project and delete prisma

--- src/app/components/FormPost.tsx
+++ src/app/components/FormPost.tsx
@@ -8,64 +8,37 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { Class } from "@prisma/client";
 
 const FormPost = () => {
-	const { register, handleSubmit } = useForm<FormInputProps>();
-	const handleCheckIn: SubmitHandler<FormInputProps> = (data) => {
-		checkInStudent(data);
-	};
-
-	const { mutate: checkInStudent, isLoading } = useMutation({
-		mutationFn: (newStudent: FormInputProps) => {
-			return axios.post("/api/students/create", newStudent);
-		},
-		onError: (error) => {
-			alert("There was a problem checking you in, please check in manually");
-			console.log(error);
-		},
-		onSuccess: () => {
-			alert("Successfully checked in");
-		},
-	});
-
-	// fetch list of classes
-	const { data: dataClasses, isLoading: isLoadingClasses } = useQuery<Class[]>({
-		queryKey: ["classes"],
-		queryFn: async () => {
-			const response = await axios.get("/api/classes");
-			return response.data;
-		},
-	});
+	const isLoadingClasses = false;
+	const dataClasses = ["Calculus I", "Calculus II", "Database", "Algebra"];
 
 	return (
 		<form
-			onSubmit={handleSubmit(handleCheckIn)}
+			onSubmit={() => {}}
 			className="flex flex-col items-center mx-20 gap-2"
 		>
 			<h3 className="self-start">Name and last name:</h3>
 			<input
 				type="text"
-				{...register("fullname", { required: true })}
 				className="py-2 w-full bg-slate-200 rounded text-center"
 				placeholder="Full name"
 			/>
 			<h3 className="self-start mt-3">Student username:</h3>
 			<input
 				type="text"
-				{...register("username", { required: true })}
 				className="py-2 w-full bg-slate-200 rounded text-center"
 				placeholder="fn10"
 			/>
 			{!isLoadingClasses ? (
 				<select
-					{...register("classId", { required: true })}
 					className="py-2 w-full bg-slate-200 rounded text-center mt-5"
 					defaultValue={""}
 				>
 					<option disabled value={""}>
 						Class:
 					</option>
-					{dataClasses?.map((item) => (
-						<option key={item.id} value={item.id}>
-							{item.classname}
+					{dataClasses?.map((item, i) => (
+						<option key={i} value={item}>
+							{item}
 						</option>
 					))}
 				</select>
